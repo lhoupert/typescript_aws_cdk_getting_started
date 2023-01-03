38e277f25744e294ffc38e29b38760e021dac769
lhoupert <10154151+lhoupert@users.noreply.github.com>
2023-01-03 13:24:43 +0000

    add encrypted s3 bucket

--- lib/pluralsight_typescript_aws_cdk-stack.ts
+++ lib/pluralsight_typescript_aws_cdk-stack.ts
@@ -1,16 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class PluralsightTypescriptAwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PluralsightTypescriptAwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new s3.Bucket(this, 'DocumentsBucket', {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
   }
 }
