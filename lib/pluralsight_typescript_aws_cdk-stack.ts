7ba11fe1d6b104ebad0a5092d810ddf7e225484b
lhoupert <10154151+lhoupert@users.noreply.github.com>
2023-01-05 17:24:41 +0000

    deploy s3 docs using the cdk

--- lib/pluralsight_typescript_aws_cdk-stack.ts
+++ lib/pluralsight_typescript_aws_cdk-stack.ts
@@ -4,7 +4,8 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Networking } from './networking'
 import { DocumentManagementAPI } from './api';
 import { Tags } from 'aws-cdk-lib';
-
+import * as s3Deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from 'path';
 
 export class PluralsightTypescriptAwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,6 +14,14 @@ export class PluralsightTypescriptAwsCdkStack extends cdk.Stack {
     const bucket = new s3.Bucket(this, 'DocumentsBucket', {
       encryption: s3.BucketEncryption.S3_MANAGED,
     });
+
+    new s3Deploy.BucketDeployment(this, 'DocumentsDeployment', {
+      sources: [ 
+        s3Deploy.Source.asset(path.join(__dirname,'..', 'documents'))
+      ],
+      destinationBucket: bucket,
+      memoryLimit: 512
+    })
 
     const networkingStack = new Networking(this, 'NetworkingConstruct', {
       maxAZs: 2
