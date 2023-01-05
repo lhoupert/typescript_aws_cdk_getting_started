5e75f7fcf1da29eae266e0c4783ba1edbd65ebcb
lhoupert <10154151+lhoupert@users.noreply.github.com>
2023-01-05 14:43:43 +0000

    create lambda function

---
+++ api/getDocuments/index.ts
@@ -0,0 +1,9 @@
+import { APIGatewayProxyEventV2, Context, APIGatewayProxyStructuredResultV2 } from 'aws-lambda'
+
+export const getDocuments = async (event: APIGatewayProxyEventV2, context: Context): Promise<APIGatewayProxyStructuredResultV2> => {
+    
+    return {
+        statusCode: 200,
+        body: 'Success'
+    }
+}
